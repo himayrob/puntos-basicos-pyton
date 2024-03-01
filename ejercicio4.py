92ffe791486405ccb87d6419b03c88a414561e2b
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-01 13:47:10 -0500

    AHI VAMOS

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+p = 2.54
+uc = input("ingrese unidades a convertir" )
+rf = float(uc)/(p)
+print (rf,"p")
+
--- ejercicios3.py
+++ ejercicios3.py
@@ -1,7 +1,7 @@
-pn =int (input("ingrese valor primera nota"))
-sn =int (input("ingrese valor segunda nota"))
-tn =int(input("ingrese valor tercera nota"))
-ct =int (input("ingrese valor cuarta nota"))
+pn =int (input("ingrese valor primera nota :"))
+sn =int (input("ingrese valor segunda nota :"))
+tn =int(input("ingrese valor tercera nota : "))
+ct =int (input("ingrese valor cuarta nota: "))
 np = int (pn+sn+tn+ct)/4
-print ("la nota promedio es", (np))
+print ("la nota promedio es :", (np))
        
