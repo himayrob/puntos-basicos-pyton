a40a378ecb68e11482efee01b63f9a2bb995c6ea
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-04 13:57:00 -0500

    terminado

---
+++ ejercicio8.py
@@ -0,0 +1,12 @@
+num = float(input("ingrese un numero: "))
+
+if num <= 0:
+    abs_numero = abs(num)
+    parte_decimal = abs_numero + int(num)
+else:
+    parte_decimal = num - int(num) 
+
+numaprox=round(parte_decimal,2)
+
+print("el decimal es", numaprox)
+
