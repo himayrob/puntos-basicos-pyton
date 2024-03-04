e49c7fd0310fc17c22fadc8073cc2d93a2ad13fd
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-04 01:18:23 -0500

    punto terminado

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,5 +1,16 @@
-h =int (input("ingrese hora actual"))
-t =int (input("ingrese tiempo a calcular"))
-tf = h+t
+def calcular_hora_futura(hora_actual, horas_por_avanzar):
 
-print("hora final", tf)
+      hora_futura = (hora_actual + horas_por_avanzar) %24
+      return hora_futura
+
+def main():
+
+    hora_actual =int(input("ingrese la hora actual(en formato 24 horas): "))
+    horas_por_avanzar = int(input("ingrese el numero de horas a avanzar: "))
+
+    hora_futura = calcular_hora_futura(hora_actual, horas_por_avanzar)
+
+    print("la hora en", horas_por_avanzar, "horas sera", hora_futura)
+
+if __name__ == "__main__":
+        main()
