914c81c1f9f9e5e6df2d1a323809dfd20ddcf2c5
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-11 14:58:21 -0500

    no he podido hacer que de 72

---
+++ ejercicio9.py
@@ -0,0 +1,9 @@
+c1 = int(input("ingresa certamen 1:"))
+c2 = int(input("ingrese certamen 2:"))
+nl = int(input("ingrese laboratorio:"))
+
+
+promedio = (60 -(nl *0.3))/(0.7)
+nota_falt = round((promedio *3) -(c1 + c2),1)
+
+print(f"esta es la nota: ",{nota_falt})
