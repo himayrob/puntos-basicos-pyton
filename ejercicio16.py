b388e0d10b5771539cb3d4e3e1f51d5b20b71985
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-13 18:39:43 -0500

    una manera simple de hacerlo con []

--- ejercicio16.py
+++ ejercicio16.py
@@ -2,5 +2,5 @@ n1 = int(input("primer numero: "))
 n2 = int(input("segundo numero: "))
 n3 = int(input("tercer numero: "))
 
-nm = min (n1,n2,n3)
-print("este es el numero menor es :",nm)
+numero_ordenado = sorted([n1,n2,n3])
+print("orden de menor a mayor",numero_ordenado)
