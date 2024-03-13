e82865170f779d3feb4bba5ecd6d31cf6cc9ad56
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-13 16:32:24 -0500

    entendi el !=

---
+++ ejercicio12.py
@@ -0,0 +1,11 @@
+def año_bisiesto(año):
+    if año %4 ==0 and (año!=0 or año%400 ==0):
+        return True
+    else:
+        return False
+    
+año =int(input("indique su año: "))
+if año_bisiesto(año):
+    print("es bisiesto")
+else:
+    print("no es bisiesto")
