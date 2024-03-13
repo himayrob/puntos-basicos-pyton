1d495e7b59f46f347d02126637b7a0af76afa8b7
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-13 17:26:59 -0500

    no se por que me marca error si hace lo que debe

---
+++ ejercicio14.py
@@ -0,0 +1,19 @@
+def palabra_larga(p1, p2):
+    lp1 = len(p1)
+    lp2 = len(p2)
+
+    if lp1 > lp2:
+        diferencia = lp1 - lp2
+        return p1,diferencia
+    elif lp2 > lp1:
+        diferencia = lp2 - lp1
+        return p2,diferencia
+    else:
+        return "ambas son iguales", 0
+
+p1 = input("ingrese palabra 1: ")
+p2 = input("ingrese palabra 2: ")
+
+palabra_larga, diferencia = palabra_larga(p1, p2)
+
+print(f"la palabra mas larga es '{palabra_larga}' y tiene diferencia de {diferencia} letras.")
