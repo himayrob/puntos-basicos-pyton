98dd955492427182e17c2ea8ee0ba6e3c71cb2f9
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-14 20:20:28 -0500

    listo

--- ejercicio18.py
+++ ejercicio18.py
@@ -1,16 +1,28 @@
-def determinarc(caracter):
-    if caracter.isalpha():
-        if caracter:isupper():
-        print("es una letra mayucula.")
+def determinarc(escribir):
+    if escribir.isalpha():
+        if escribir.isupper():
+            print("es una letra mayucula.")
         else:
-        print("es una letra minuscula")
-    elif caracter.isdigit():
+            print("es una letra minuscula")
+    elif escribir.isdigit():
         print("es un numero.")
     else:
         print("no es ni letra ni numero.")
 
-caracter = input("ingrese un caracter")  
-determinarc(caracter)
+escribir = input("ingrese un escribir")  
+determinarc(escribir)
 
 
+# def determinarc(caracter):
+#     if caracter.isalpha():
+#         if caracter.isupper():
+#             print("Es una letra mayúscula.")
+#         else:
+#             print("Es una letra minúscula.")
+#     elif caracter.isdigit():
+#         print("Es un número.")
+#     else:
+#         print("No es ni una letra ni un número.")
 
+# caracter = input("Ingrese un caracter: ")  
+# determinarc(caracter)
