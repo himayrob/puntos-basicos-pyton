cf6fecb0a3226b8939a743a989bbb37a52927985
himayrob <160776152+himayrob@users.noreply.github.com>
2024-04-08 20:55:41 -0500

    feat:  ejercicio 24 sin terminar

---
+++ ejercicio23.py
@@ -0,0 +1,8 @@
+#potencia de dos
+potencia=int(input("ingrese potencia deseada"))
+
+
+for i in range(0, potencia+1 ):
+   resultado = 2**i
+     
+   print(f"{2} a la {i} = {resultado}")   
---
+++ ejercicio24.py
@@ -0,0 +1,8 @@
+#potencia de dos
+num1 =int(input("ingrese numero 1: "))
+num2 =int(input("ingrese numero 2: "))
+
+for i in range(num1,num2 ):
+   
+     
+   print(f"{2} a la {i} = {resultado}")   
